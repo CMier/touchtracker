d919328c2865c431849ced8aa7b3ecfa809d474c
CMier <caesarmier@csu.fullerton.edu>
2020-04-17 18:59:22 -0700

    Add files via upload

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by CSUFTitan on 4/8/20.
+//  Copyright © 2020 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
